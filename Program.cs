b9a08c06f5d1b72ad1b3137f01a184713ca86a97
ETEC Adolpho Berezin <colatti2017@gmail.com>
2023-03-31 20:07:46 -0300

    exercicioresolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+string Nome,Sobrenome;
+Console.WriteLine();
+Console.Write("digite seu nome....");
+Nome = Console.ReadLine()!;
+Console.WriteLine();
+Console.Write("digite seu sobrenome ...");
+Sobrenome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine($"nome completo: {Nome} {Sobrenome}");
+Console.WriteLine($"nome de catalago {Sobrenome.ToUpper()}, {Nome}");
+Console.WriteLine();
+
